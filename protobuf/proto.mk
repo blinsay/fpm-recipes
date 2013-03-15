11cb290186ab259e8acbf8928dc7444eb7790472
Ben Linsay <benl@boundary.com>
2013-03-15 14:08:59 -0700

    Add a makefile for protobuf

---
+++ protobuf/proto.mk
@@ -0,0 +1,19 @@
+NAME = protobuf_$(VERSION)
+SOURCE_URL = https://protobuf.googlecode.com/files/protobuf-$(VERSION).tar.gz 
+
+PROTO_INSTALL_DIR = /opt/protobuf/
+FPM_SOURCE = dir
+
+PACKAGE_URL = https://code.google.com/p/protobuf/
+PACKAGE_DESCRIPTION = Google Protocol Buffers
+
+include ../../include/base.mk
+
+export DESTDIR
+
+extract: fetch default_build
+	cd $(BUILDDIR); tar xzf $(CACHEDIR)/$(notdir $(SOURCE_URL))
+
+build: extract
+	cd $(BUILDDIR)/protobuf-$(VERSION); \
+	./configure --prefix=$(PROTO_INSTALL_DIR)/$(VERSION)/ && make install
